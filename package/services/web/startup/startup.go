379733b5576738b0e5eda098ce17513343e13607
Raphael Jacob <r.jacob2002@gmail.com>
2021-01-09 17:46:37 +0100

    Service web: no autostart

--- package/services/web/startup/startup.go
+++ package/services/web/startup/startup.go
@@ -3,12 +3,12 @@ package web_startup
 import (
 	"github.com/servicemngr/core/package/instance"
 	"github.com/servicemngr/core/package/manifest"
-	consts "github.com/servicemngr/servicemngr-svcs-base/package/services/web/consts"
 )
 
 func Startup(configurator manifest.Configurator) []instance.ID {
-	return []instance.ID{{
-		Name:     consts.SERVICE_NAME,
-		Instance: instance.NON_INSTANCE_NAME,
-	}}
+	/*
+		Proposal:
+		Via configuration: Set web hosting directories. If set, start instance automatically.
+	*/
+	return []instance.ID{}
 }
